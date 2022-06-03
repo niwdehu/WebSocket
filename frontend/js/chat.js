3a319f7238cc41fa6625dd1d84ec30472ae68297
Edwin Huaman <ed_hu@hotmail.com>
2022-06-03 18:41:44 -0500

    modif

--- frontend/js/chat.js
+++ frontend/js/chat.js
@@ -50,7 +50,7 @@ function selectUser(userName) {
         render(newMessages.get(userName), userName);
     }
     $('#selectedUserId').html('');
-    $('#selectedUserId').append('Chat with ' + userName);
+    $('#selectedUserId').append('Chat con ' + userName);
 }
 
 function fetchAll() {
@@ -62,7 +62,7 @@ function fetchAll() {
             usersTemplateHTML = usersTemplateHTML + '<a href="#" onclick="selectUser(\'' + users[i] + '\')"><li class="clearfix">\n' +
                 '                <img src="' + urlSrc + '" width="55px" height="55px" alt="avatar" />\n' +
                     '                <div class="about">\n' +
-                    '                    <div id="userNameAppender_' + users[i] + '" class="name">' + users[i] + '</div>\n' +
+                    '                    <div id="userNameAppender_' + users[i] + '" class="name" style="color:#dadbd5;">' + users[i] + '</div>\n' +
                     '                    <div class="status">\n' +
                     '                        <i class="fa fa-circle offline"></i>\n' +
                     '                    </div>\n' +
--- src/main/java/com/javamaster/WebSocketProjectApplication.java
+++ src/main/java/com/javamaster/WebSocketProjectApplication.java
@@ -2,9 +2,10 @@ package com.javamaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebSocketProjectApplication {
+public class WebSocketProjectApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(WebSocketProjectApplication.class, args);
